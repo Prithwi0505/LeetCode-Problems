74e7ec3fcf42d3f51781f00a5361a94ae33d4ee6
Prithwiraj Paul <prithwiraj.paul5@gmail.com>
2026-07-10 12:48:52 +0530

    Time: 67 ms (87.59%), Space: 125.3 MB (87.59%) - LeetHub

---
+++ 3457-eat-pizzas/3457-eat-pizzas.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long maxWeight(int[] pizzas) {
+        Arrays.sort(pizzas);
+        int n = pizzas.length;
+        int days = n/4;
+
+        int odd = (days+1) / 2;
+        int even = days / 2;
+
+        long sum = 0;
+
+        int left = 0;
+        int right = n-1;
+
+        for(int i = 1; i <= odd; i++){
+            sum += pizzas[right];
+            right--;
+            left += 3;
+        }
+
+        for(int i = 1; i <= even; i++){
+            right--;
+            sum += pizzas[right];
+            right--;
+            left += 2;
+        }
+        return sum;
+    }
+}
