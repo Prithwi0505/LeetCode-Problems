54dde2c5de106ae60e1e0fa4e0690e95271281dc
Prithwiraj Paul <prithwiraj.paul5@gmail.com>
2026-07-11 09:14:17 +0530

    Time: 11 ms (42.86%), Space: 47.6 MB (21.53%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -5,15 +5,15 @@ class Solution {
             map.put(heights[i], names[i]);
         }
         Arrays.sort(heights);
-        for(int i = 0; i < heights.length/2; i++){
-            int temp = heights[i];
-            heights[i] = heights[heights.length-1-i];
-            heights[heights.length-1-i] = temp;
-        }
+        // for(int i = 0; i < heights.length/2; i++){
+        //     int temp = heights[i];
+        //     heights[i] = heights[heights.length-1-i];
+        //     heights[heights.length-1-i] = temp;
+        // }
 
         String[] res = new String[names.length];
         for(int i = 0; i < res.length; i++){
-            res[i] = map.get(heights[i]);
+            res[i] = map.get(heights[heights.length-1-i]);
         }
         return res;
     }
